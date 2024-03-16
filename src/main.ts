0394f073a9180e6d0a22b5c5bc4b8ae82db3fae0
nayefserag1 <nayf.serag@mahaseel.net>
2024-03-16 22:42:27 +0200

    display os hostname name

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from 'src/app.module';
 import { HttpExceptionFilter } from './logging/http-exception.filter';
 import { setupSwagger } from './config/swagger.config';
 import { ConfigService } from '@nestjs/config';
-
+import * as os from 'os';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
@@ -12,6 +12,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   setupSwagger(app);
-  await app.listen(port);
+  await app.listen(port, () => {
+    console.log(`Application is running on: ${port}`);
+    console.log(os.hostname());
+  });
 }
 bootstrap();
