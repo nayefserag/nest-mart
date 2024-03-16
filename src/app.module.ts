ea522e4c1dff0354cf0daef43b95edfa0a462406
nayefserag1 <nayf.serag@mahaseel.net>
2024-03-16 22:33:25 +0200

    edit postgress configrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,31 +3,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './config/orm.config';
 import { ProductsController } from './components/products/products.controller';
 import { ProductsModule } from './components/products/products.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './logging/logging.interceptor';
 import { ProductsService } from './components/products/products.service';
 import { ProductsRepository } from './repos/products.repo';
 import { Product } from './entities/product.entity';
-import  configrations  from './config/configrations';
 @Module({
   imports: [
-    TypeOrmModule.forRoot(ormConfig),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-      load: [configrations],
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: ormConfig,
     }),
     TypeOrmModule.forFeature([Product]),
     ProductsModule,
   ],
-  controllers: [ ProductsController],
+  controllers: [ProductsController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
     },
-     ProductsService , ProductsRepository
+    ProductsService,
+    ProductsRepository,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from 'src/app.module';
 import { HttpExceptionFilter } from './logging/http-exception.filter';
 import { setupSwagger } from './config/swagger.config';
-import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  dotenv.config();
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   setupSwagger(app);
-  await app.listen(process.env.PORT);
+  await app.listen(port);
 }
 bootstrap();
